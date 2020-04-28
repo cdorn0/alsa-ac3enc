7e3fd1aa7dd5bf748dd8f0f5f6f96d522a15b5ff
cdorn <christian.dorn@tng-project.de>
2020-04-28 09:28:05 +0200

    Added support for rewind of audiostream
    In order to support pulseaudio's buffer rewind pcm buffer has been
    converted to a ringbuffer and it's buffer size was increased to
    snd_pcm_hw_params_get_buffer_size_max. AC3 encoder input buffer
    is seperated from pcm buffer.

--- pcm_ac3.c
+++ pcm_ac3.c
@@ -16,8 +16,6 @@
  *  You should have received a copy of the GNU Lesser General Public License
  *  along with alsa-ac3enc.  If not, see <https://www.gnu.org/licenses/>.
 */
-
-
 #include <string.h>
 
 #define __USE_XOPEN
@@ -31,8 +29,6 @@
 
 #define IEC958_HEADER_SIZE  8
 #define IEC958_FRAME_SIZE   6144
-
-static const float zeros[6 * A52_SAMPLES_PER_FRAME] = {};
 
 static void ac3enc_fillIEC958Header(unsigned char *buf, int ac3outsize) {
 #if __BYTE_ORDER == __BIG_ENDIAN
@@ -121,37 +117,65 @@ inline int ac3enc_convert(AftenContext* c, const float *input, uint8_t *output) 
     return aften_encode_frame(c, (unsigned char *) output, input, A52_SAMPLES_PER_FRAME);
 }
 
-int ac3enc_destroy(AftenContext* c, uint8_t *output) {
-    int result = 0;
-
-    if (output) {
-        /* Create a dummy frame of silence */
-        int outsize = ac3enc_convert(c, zeros, output);
-        if (outsize >= 0) {
-            ac3enc_fillIEC958Header(output, outsize);
-
-            result = outsize;
-        }
+int ac3enc_destroy(AftenContext* c) {
+    if (!c) {
+        return -EINVAL;
     }
 
     free(c);
 
-    return result;
+    return 0;
+}
+
+struct pcm_buffer {
+    float* buffer;
+    snd_pcm_uframes_t size;
+    snd_pcm_uframes_t index;
+};
+
+static inline void ac3plug_pcm_buffer_rewind(struct pcm_buffer* pcmbuf, snd_pcm_uframes_t count) {
+    int idx = (pcmbuf->index - count) % pcmbuf->size;
+
+    if (idx < 0)
+        pcmbuf->index = pcmbuf->size + idx;
+    else
+        pcmbuf->index = idx;
+}
+
+static inline void ac3plug_pcm_buffer_push(struct pcm_buffer* pcmbuf, float value) {
+    pcmbuf->buffer[pcmbuf->index++] = value;
+    /* Wrap around buffer size */
+    pcmbuf->index %= pcmbuf->size;
+}
+
+static void ac3plug_pcm_buffer_get(struct pcm_buffer* pcmbuf, float* output, snd_pcm_uframes_t size) {
+    int offset = pcmbuf->index - size;
+
+    /* Wrap around buffer size */
+    if (offset < 0) {
+        memcpy(output, &pcmbuf->buffer[pcmbuf->size + offset] , (-offset)*sizeof(float));
+        memcpy(output-offset, &pcmbuf->buffer[0], (size+offset)*sizeof(float));
+    } else {
+        memcpy(output, &pcmbuf->buffer[offset], size*sizeof(float));
+    }
 }
 
 struct ac3plug_info {
     snd_pcm_extplug_t ext;
 
+    snd_pcm_uframes_t pcm_buffer_max_size;
+
     AftenContext* afctx;
     unsigned int threads;
 
-    float pcm_buffer[A52_SAMPLES_PER_FRAME * 6];
-    int16_t ac3_buffer[IEC958_FRAME_SIZE];
-    snd_pcm_uframes_t bufpos;
+    struct pcm_buffer pcmbuf;
+    int16_t iec958_buffer[IEC958_FRAME_SIZE];
+    snd_pcm_uframes_t iec958_bufpos;
+
+    snd_pcm_uframes_t src_next_offset;
 };
 
-static inline void *area_addr(const snd_pcm_channel_area_t *area,
-                              snd_pcm_uframes_t offset)
+static inline void *area_addr(const snd_pcm_channel_area_t *area, snd_pcm_uframes_t offset)
 {
     unsigned int bitofs = area->first + area->step * offset;
     return (char *) area->addr + bitofs / 8;
@@ -180,6 +204,7 @@ static inline float get_float(const snd_pcm_channel_area_t *area, snd_pcm_uframe
     return 0;
 }
 
+float inbuffer[6 * A52_SAMPLES_PER_FRAME];
 static inline void put_s16(const snd_pcm_channel_area_t *area, snd_pcm_uframes_t offset, int16_t value)
 {
     int16_t* sample16 = (int16_t*)area_addr(area, offset);
@@ -193,49 +218,76 @@ static snd_pcm_sframes_t ac3plug_transfer(snd_pcm_extplug_t *ext,
 {
     struct ac3plug_info *ac3plug = (struct ac3plug_info*) ext;
 
-    /* Buffer must not be overfilled, limit count of the samples copied to A52_SAMPLES_PER_FRAME */
-    snd_pcm_uframes_t remaining = A52_SAMPLES_PER_FRAME - ac3plug->bufpos;
+    /* Try to detect a rewind of the buffer (LIMIT: If rewound to zero, it is not detected) */
+    if ((src_offset > 0) && (src_offset < ac3plug->src_next_offset)) {
+        snd_pcm_uframes_t difference = ac3plug->src_next_offset - src_offset;
+        ac3plug_pcm_buffer_rewind(&ac3plug->pcmbuf, ext->channels*difference);
+
+        /* If rewound drop recent IEC958 frame and convert new one. This is not an optimal solution as frames are dropped (TODO: is it allowed in spec?) */
+        ac3plug->iec958_bufpos = 0;
+    }
+
+    /* Output Buffer must not be overfilled, limit count of the samples copied to A52_SAMPLES_PER_FRAME
+     * TODO: To save computational power wrap in while and encode as chunks
+     * */
+    snd_pcm_uframes_t remaining = A52_SAMPLES_PER_FRAME - ac3plug->iec958_bufpos;
     if (size > remaining)
         size = remaining;
 
-    int srcbufidx = ext->channels * ac3plug->bufpos;
-    int dstbufidx = 2 * ac3plug->bufpos;
+    /* Save the the position where the next transfer should take place */
+    ac3plug->src_next_offset = src_offset + size;
+
+    /* If the buffer position is zero, encode the last A52_SAMPLES_PER_FRAME samples in the PCM buffer */
+    if (ac3plug->iec958_bufpos == 0) {
+        ac3plug_pcm_buffer_get(&ac3plug->pcmbuf, inbuffer, ext->channels*A52_SAMPLES_PER_FRAME);
+
+        int outsize = ac3enc_convert(ac3plug->afctx, inbuffer, (uint8_t *) ac3plug->iec958_buffer + IEC958_HEADER_SIZE);
+        if (outsize < 0) {
+            SNDERR("AC3 conversion failed");
+
+            return -EIO;
+        }
+
+        ac3enc_fillIEC958Header((uint8_t *) ac3plug->iec958_buffer, outsize);
+    }
+
+    int dstbufidx = 2 * ac3plug->iec958_bufpos;
 
     for (snd_pcm_uframes_t i = 0; i < size; i++) {
         /* Read samples to pcm buffer according to channel count*/
         switch (ext->channels) {
             case 1:
-                ac3plug->pcm_buffer[srcbufidx++] = get_float(&src_areas[0], i + src_offset, ext->format);
+                ac3plug_pcm_buffer_push(&ac3plug->pcmbuf, get_float(&src_areas[0], i + src_offset, ext->format));
                 break;
             case 2:
-                ac3plug->pcm_buffer[srcbufidx++] = get_float(&src_areas[0], i + src_offset, ext->format);
-                ac3plug->pcm_buffer[srcbufidx++] = get_float(&src_areas[1], i + src_offset, ext->format);
+                ac3plug_pcm_buffer_push(&ac3plug->pcmbuf, get_float(&src_areas[0], i + src_offset, ext->format));
+                ac3plug_pcm_buffer_push(&ac3plug->pcmbuf, get_float(&src_areas[1], i + src_offset, ext->format));
                 break;
             case 3:
-                ac3plug->pcm_buffer[srcbufidx++] = get_float(&src_areas[0], i + src_offset, ext->format);
-                ac3plug->pcm_buffer[srcbufidx++] = get_float(&src_areas[1], i + src_offset, ext->format);
-                ac3plug->pcm_buffer[srcbufidx++] = get_float(&src_areas[2], i + src_offset, ext->format);
+                ac3plug_pcm_buffer_push(&ac3plug->pcmbuf, get_float(&src_areas[0], i + src_offset, ext->format));
+                ac3plug_pcm_buffer_push(&ac3plug->pcmbuf, get_float(&src_areas[1], i + src_offset, ext->format));
+                ac3plug_pcm_buffer_push(&ac3plug->pcmbuf, get_float(&src_areas[2], i + src_offset, ext->format));
                 break;
             case 4:
-                ac3plug->pcm_buffer[srcbufidx++] = get_float(&src_areas[0], i + src_offset, ext->format);
-                ac3plug->pcm_buffer[srcbufidx++] = get_float(&src_areas[1], i + src_offset, ext->format);
-                ac3plug->pcm_buffer[srcbufidx++] = get_float(&src_areas[2], i + src_offset, ext->format);
-                ac3plug->pcm_buffer[srcbufidx++] = get_float(&src_areas[3], i + src_offset, ext->format);
+                ac3plug_pcm_buffer_push(&ac3plug->pcmbuf, get_float(&src_areas[0], i + src_offset, ext->format));
+                ac3plug_pcm_buffer_push(&ac3plug->pcmbuf, get_float(&src_areas[1], i + src_offset, ext->format));
+                ac3plug_pcm_buffer_push(&ac3plug->pcmbuf, get_float(&src_areas[2], i + src_offset, ext->format));
+                ac3plug_pcm_buffer_push(&ac3plug->pcmbuf, get_float(&src_areas[3], i + src_offset, ext->format));
                 break;
             case 5:
-                ac3plug->pcm_buffer[srcbufidx++] = get_float(&src_areas[0], i + src_offset, ext->format);
-                ac3plug->pcm_buffer[srcbufidx++] = get_float(&src_areas[4], i + src_offset, ext->format);
-                ac3plug->pcm_buffer[srcbufidx++] = get_float(&src_areas[1], i + src_offset, ext->format);
-                ac3plug->pcm_buffer[srcbufidx++] = get_float(&src_areas[2], i + src_offset, ext->format);
-                ac3plug->pcm_buffer[srcbufidx++] = get_float(&src_areas[3], i + src_offset, ext->format);
+                ac3plug_pcm_buffer_push(&ac3plug->pcmbuf, get_float(&src_areas[0], i + src_offset, ext->format));
+                ac3plug_pcm_buffer_push(&ac3plug->pcmbuf, get_float(&src_areas[4], i + src_offset, ext->format));
+                ac3plug_pcm_buffer_push(&ac3plug->pcmbuf, get_float(&src_areas[1], i + src_offset, ext->format));
+                ac3plug_pcm_buffer_push(&ac3plug->pcmbuf, get_float(&src_areas[2], i + src_offset, ext->format));
+                ac3plug_pcm_buffer_push(&ac3plug->pcmbuf, get_float(&src_areas[3], i + src_offset, ext->format));
                 break;
             case 6:
-                ac3plug->pcm_buffer[srcbufidx++] = get_float(&src_areas[0], i + src_offset, ext->format);
-                ac3plug->pcm_buffer[srcbufidx++] = get_float(&src_areas[4], i + src_offset, ext->format);
-                ac3plug->pcm_buffer[srcbufidx++] = get_float(&src_areas[1], i + src_offset, ext->format);
-                ac3plug->pcm_buffer[srcbufidx++] = get_float(&src_areas[2], i + src_offset, ext->format);
-                ac3plug->pcm_buffer[srcbufidx++] = get_float(&src_areas[3], i + src_offset, ext->format);
-                ac3plug->pcm_buffer[srcbufidx++] = get_float(&src_areas[5], i + src_offset, ext->format);
+                ac3plug_pcm_buffer_push(&ac3plug->pcmbuf, get_float(&src_areas[0], i + src_offset, ext->format));
+                ac3plug_pcm_buffer_push(&ac3plug->pcmbuf, get_float(&src_areas[4], i + src_offset, ext->format));
+                ac3plug_pcm_buffer_push(&ac3plug->pcmbuf, get_float(&src_areas[1], i + src_offset, ext->format));
+                ac3plug_pcm_buffer_push(&ac3plug->pcmbuf, get_float(&src_areas[2], i + src_offset, ext->format));
+                ac3plug_pcm_buffer_push(&ac3plug->pcmbuf, get_float(&src_areas[3], i + src_offset, ext->format));
+                ac3plug_pcm_buffer_push(&ac3plug->pcmbuf, get_float(&src_areas[5], i + src_offset, ext->format));
                 break;
 
             default:
@@ -243,24 +295,14 @@ static snd_pcm_sframes_t ac3plug_transfer(snd_pcm_extplug_t *ext,
         }
 
         /* Write out encoded samples to IEC958 (int16 stereo) */
-        put_s16(&dst_areas[0], i + dst_offset, ac3plug->ac3_buffer[dstbufidx++]);
-        put_s16(&dst_areas[1], i + dst_offset, ac3plug->ac3_buffer[dstbufidx++]);
+        put_s16(&dst_areas[0], i + dst_offset, ac3plug->iec958_buffer[dstbufidx++]);
+        put_s16(&dst_areas[1], i + dst_offset, ac3plug->iec958_buffer[dstbufidx++]);
     }
 
-    ac3plug->bufpos += size;
+    ac3plug->iec958_bufpos += size;
 
-    if (ac3plug->bufpos == A52_SAMPLES_PER_FRAME) {
-        int outsize = ac3enc_convert(ac3plug->afctx, ac3plug->pcm_buffer,
-                                     (uint8_t *) ac3plug->ac3_buffer + IEC958_HEADER_SIZE);
-        if (outsize < 0) {
-            SNDERR("AC3 conversion failed");
-
-            return -EIO;
-        }
-
-        ac3enc_fillIEC958Header((uint8_t *) ac3plug->ac3_buffer, outsize);
-
-        ac3plug->bufpos = 0;
+    if (ac3plug->iec958_bufpos == A52_SAMPLES_PER_FRAME) {
+        ac3plug->iec958_bufpos = 0;
     }
 
     return size;
@@ -280,6 +322,17 @@ static int ac3plug_init(snd_pcm_extplug_t *ext)
         return -EINVAL;
     }
 
+    ac3plug->pcmbuf.index = 0;
+    ac3plug->pcmbuf.size = ac3plug->pcm_buffer_max_size;
+    ac3plug->pcmbuf.buffer = calloc(ac3plug->pcmbuf.size * 6, sizeof(float));
+
+    if (!ac3plug->pcmbuf.buffer) {
+        SNDERR("Error allocating memory for PCM buffer");
+        return -ENOMEM;
+    }
+
+    memset(ac3plug->pcmbuf.buffer, 0x00, ac3plug->pcmbuf.size);
+
     ac3plug->afctx = ac3enc_create(ac3plug->threads, ext->rate, ext->channels);
 
     if (!ac3plug->afctx) {
@@ -287,15 +340,9 @@ static int ac3plug_init(snd_pcm_extplug_t *ext)
         return -ENOMEM;
     }
 
-    /* Create a dummy frame of silence */
-    int outsize = ac3enc_convert(ac3plug->afctx, zeros, (uint8_t *) ac3plug->ac3_buffer);
-    if (outsize < 0) {
-        SNDERR("AC3 conversion failed");
+    ac3plug->src_next_offset = 0;
+    ac3plug->iec958_bufpos = 0;
 
-        return -EIO;
-    }
-
-    ac3enc_fillIEC958Header((uint8_t *) ac3plug->ac3_buffer, outsize);
 
     return 0;
 }
@@ -304,7 +351,7 @@ static int ac3plug_close(snd_pcm_extplug_t *ext)
 {
     struct ac3plug_info *ac3plug = (struct ac3plug_info*)ext;
 
-    ac3enc_destroy(ac3plug->afctx, NULL);
+    ac3enc_destroy(ac3plug->afctx);
     ac3plug->afctx = NULL;
     return 0;
 }
@@ -373,7 +420,7 @@ SND_PCM_PLUGIN_DEFINE_FUNC(ac3)
     snd_config_iterator_t i, next;
     snd_config_t *slave = NULL;
 
-    unsigned int threads = 0;
+    unsigned int threads = 2;
 
     struct ac3plug_info *ac3plug;
     int err;
@@ -433,13 +480,13 @@ SND_PCM_PLUGIN_DEFINE_FUNC(ac3)
 
     err = snd_pcm_extplug_create(&ac3plug->ext, name, root, slave, stream, mode);
     if (err < 0) {
-        ac3enc_destroy(ac3plug->afctx, NULL);
+        ac3enc_destroy(ac3plug->afctx);
         free(ac3plug);
         return err;
     }
 
-    static const int channels[6] = {1, 2, 3, 4, 5, 6};
-    static const int formats[3] = {SND_PCM_FORMAT_FLOAT, SND_PCM_FORMAT_S32, SND_PCM_FORMAT_S16};
+    static const unsigned int channels[6] = {1, 2, 3, 4, 5, 6};
+    static const unsigned int formats[3] = {SND_PCM_FORMAT_FLOAT, SND_PCM_FORMAT_S32, SND_PCM_FORMAT_S16};
 
     snd_pcm_extplug_set_param_list(&ac3plug->ext, SND_PCM_EXTPLUG_HW_CHANNELS, 6, channels);
     snd_pcm_extplug_set_param_list(&ac3plug->ext, SND_PCM_EXTPLUG_HW_FORMAT, 3, formats);
@@ -448,6 +495,29 @@ SND_PCM_PLUGIN_DEFINE_FUNC(ac3)
     snd_pcm_extplug_set_slave_param(&ac3plug->ext, SND_PCM_EXTPLUG_HW_FORMAT, SND_PCM_FORMAT_S16);
 
     *pcmp = ac3plug->ext.pcm;
+
+    snd_pcm_hw_params_t* params;
+
+    if ((err = snd_pcm_hw_params_malloc(&params)) < 0)
+        return err;
+
+    if ((err = snd_pcm_hw_params_any(ac3plug->ext.pcm, params)) < 0) {
+        SNDERR("Cannot get slave hw_params");
+        free (params);
+
+        return err;
+    }
+
+    snd_pcm_uframes_t buffer_max;
+    if ((err = snd_pcm_hw_params_get_buffer_size_max(params, &buffer_max)) < 0) {
+        SNDERR("Cannot get max buffer_size");
+        free (params);
+
+        return err;
+    }
+
+    ac3plug->pcm_buffer_max_size = buffer_max;
+
     return 0;
 }
 
